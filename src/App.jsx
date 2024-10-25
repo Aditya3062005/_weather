a74a614c1f3039fe365d24757c8548d3d2a911d2
Aditya3062005 <165989097+Aditya3062005@users.noreply.github.com>
2024-10-25 14:58:23 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import WeatherGet from './Weather/WeatherGet'
+
+const App = () => {
+  return (
+    <div>
+      <WeatherGet/>
+    </div>
+  )
+}
+
+export default App
